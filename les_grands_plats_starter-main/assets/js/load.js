6b723600582692287f62875af76ac3359dc3b301
lthihuongn <lilou33127v@gmail.com>
2025-03-27 15:06:28 +0100

    commit mais ca marche toujours pas...

--- les_grands_plats_starter-main/assets/js/load.js
+++ les_grands_plats_starter-main/assets/js/load.js
@@ -1,10 +1,9 @@
-//import { fetchRecipes } from "./fetch.js";
+import { fetchRecipes } from "./fetch.js";
 import { searchRecipes, createSearchWrapper } from "./search.js";
 import { filterPlats } from "./filter.js";
 
 document.addEventListener('DOMContentLoaded', () => {
-    console.log("bonjour");
-    //fetchRecipes();
+    fetchRecipes();
     filterPlats();
 
 
